24c5dce21d9e3bffd1ef5814e10df965aefa218f
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-28 05:27:59 +0200

    get JSON (done)

--- API DEMO/ViewController.swift
+++ API DEMO/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let url = URL(string:"http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=358a1c0e3636815b8188f88a194581b6")
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                print("ERROR - \(error)")
+            } else {
+                if let content = data {
+                    do{
+                        let jsonResult = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        print(jsonResult)
+                    } catch {
+                        print("JSON Processing Failed")
+                    }
+                  
+                }
+                
+            }
+            
+        }
+        task.resume()
+        
+       
     }
-
 
 }
 
